2bb5966e4d20c1181cc43065e3f15edb5f5f4c14
guilhermem0101 <guilherme.sou@hotmail.com>
2022-03-24 13:07:27 -0300

    att

--- portifolio/src/app/app.module.ts
+++ portifolio/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { TesteComponent } from './teste/teste.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    RodapeComponent
+    RodapeComponent,
+    InicioComponent,
+    TesteComponent
   ],
   imports: [
     BrowserModule,
